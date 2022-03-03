37a09d7b2f05ec696e68e8f5d69c4a8d2e2bfff5
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-03-02 19:56:41 -0800

    added db connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2')
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,18 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    //  your mysql username,
+    user: 'root',
+    password: 'password',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 //Default response for any other request(Not Found)
 app.use((req,res) => {
